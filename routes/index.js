a0df649d2be67f791bfbac2deebca812c1855d3e
cksrud1627 <cksrud1627@naver.com>
2021-06-03 16:51:59 +0900

    .

--- routes/index.js
+++ routes/index.js
@@ -2,41 +2,78 @@ const scheduler = require("../models/scheduler");
 const express = require('express');
 const logger = require('../lib/logger');
 const { validator } = require('../middlewares/validator');
+
 const router = express.Router();
 
-router.get('/', async (req,res,next) => {
-  const data = await scheduler.getCalendar(req.query.year , req.query.month);
-  console.log(data);
-   res.render('main',data);
-})
-
-// axios - ajax
-router.route('/schedule')
 /** 스케줄 조회 */
-      .get((req,res,next) => {
-        const stamp = req.query.stamp;
-        if (!stamp) {
-          return res.send("<script>alert('잘못된 접근입니다.');yh.layer.close();</script>");
-        }
-        const data = {
-          stamp,
-          colors : scheduler.getColors(),
-        };
-          res.render("form",data);
-      })
-      /** 스케줄 등록 */
-      .post(validator, async (req,res,next) => {
-        const result = await scheduler.add(req.body);
+router.get('/', async (req, res, next) => {
+	const data = await scheduler.getCalendar(req.query.year, req.query.month);
+		data.todaySchedules = await scheduler.getTodaySchedules();
 
-       return res.json({success: result})
-      })
-      /** 스케줄 수정 */
-      .patch((req,res,next) => {
+	res.render('main', data);
+});
 
-      })
-      /** 스케줄 삭제 */
-      .delete((req,res,next) => {
+router.route('/schedule')
+		/** 스케줄 등록 양식 */
+		.get((req, res, next) => {
+			const stamp = req.query.stamp;
+			let date = "";
+			if (!stamp) {
+				date = scheduler.getDate(stamp);
+	    }
 
-      });
+			const data = {
+				stamp,
+				colors : Object.keys(scheduler.getColors()),
+				date,
+			};
+			res.render("form", data);
+		})
+		/** 스케줄 등록, 수정 */
+		.post(validator, async (req, res, next) => {
+			const result = await scheduler.add(req.body);
+
+			return res.json({success: result})
+		})
+
+		/** 스케줄  색상 수정 */
+		.patch(async(req, res, next) => {
+        const result = await scheduler.changeColor(req.body.period, req.body.prevColor, req.body.color);
+
+		return res.send("");
+		})
+		/** 스케줄 삭제 */
+		.delete(async(req, res, next) => {
+        const result = await scheduler.delete(req,query.period);
+				return res.json({ success : result });
+		});
+
+/** 스케줄 조회 */
+router.get("/schedule/view/:stamp/:color", async (req,res,next) => {
+	const data = await scheduler.getSchedule(req.params.stamp, req.params.color);
+  data.colors = Object.keys(scheduler.getColors());
+	return res.render("view", data);
+});
+
+/** 스케줄 수정 */
+router.get("/schedule/:period/:color", async (req, res, next) => {
+	const data = await scheduler.getInfo(req.params.period, req.params.color);
+	data.period = req.params.period;
+	data.colors =  Object.keys(scheduler.getColors());
+
+	return res.render("form", data);
+});
+
+/** 오늘 스케줄 확인 */
+router.route("/schedule/today")
+.get(async (req,res,next) => {
+	const list = await scheduler.getTodaySchedules();
+	return res.render("today", {list});
+})
+.patch(async (req,res,next) => {
+	const result = await scheduler.confirmTodaySchedule(req.body.isChecked);
+
+	return res.json({ success : result });
+});
 
 module.exports = router;
